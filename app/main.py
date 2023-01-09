396065e1c49a496eb32f9b7c4d3f1f9f310286ac
Chikaki Ryotaro <r.chikaki@gmail.com>
2023-01-09 09:33:48 +0900

    # Stage 3: Handle exit codes

--- app/main.py
+++ app/main.py
@@ -7,32 +7,18 @@ import sys
 # Along the way, we'll learn about chroot, kernel namespaces, the docker registry API and much more.
 
 
-# Stage 2: Wireup stdout & stderr
-# You'll now pipe the program's stdout and stderr to the parent process.
-# Like the last stage, the tester will run your program like this:
-# mydocker run ubuntu:latest /usr/local/bin/docker-explorer echo hey
-# To test this behaviour locally, you could use the echo + echo_stderr commands that docker-explorer exposes. Run docker-explorer --help to view usage.
-
-# Usage: docker-explorer COMMAND [ARGS]
-
-# Commands:
-#   echo <something>        : prints <something> to stdour
-#   echo_stderr <something> : prints <something> to stderr
-#   mypid                   : prints the process's PID
-#   ls <dir>                : lists files in <dir>
-#   touch <path>            : create a new file at <path>
-#   exit <exit_code>        : exit with <exit_code>
-
-# Examples:
-#   docker-explorer echo hey
-#   docker-explorer ls /
-
-# If you've got any logs or print statements in your code, make sure to remove them. The tester can't differentiate between debug logs and the actual output!
-
+# Stage 3: Handle exit codes
+# In this stage, you'll need to relay the program's exit code to the parent process.
+# If the program you're executing exits with exit code 1, your program should exit with exit code 1 too.
+# To test this behaviour locally, you could use the exit command that docker-explorer exposes. Run docker-explorer --help to view usage.
+# Just like the previous stage, the tester will run your program like this:
+# mydocker run ubuntu:latest /usr/local/bin/docker-explorer exit 1
 
 def main():
     command = sys.argv[3]
+    #  /usr/local/bin/docker-explorer
     args = sys.argv[4:]
+    # ['exit', '29']
 
     # Run a command as a subprocess, capturing its output, and then printing the output to the console.
     completed_process = subprocess.run([command, *args], capture_output=True)
@@ -40,6 +26,10 @@ def main():
     # Wire stdout to sys.stdout/stderr to sys.stderr
     print(completed_process.stdout.decode("utf-8"), end='', file=sys.stdout)
     print(completed_process.stderr.decode("utf-8"), end='',  file=sys.stderr)
+
+    # Return exit code if it is not equal to zero.
+    if completed_process.returncode != 0:
+        sys.exit(completed_process.returncode)
 
 
 if __name__ == "__main__":
